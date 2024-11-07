40be1f832f2c45103faabc3857d9ad30cfb077dd
Geert Peeters <geertpeeters@zoho.com>
2024-11-08 00:16:08 +0100

    Updated output

--- export-schematic.sh
+++ export-schematic.sh
@@ -10,5 +10,5 @@ echo $SCHEMATIC_FILE
 # Run KiCad CLI commands to export schematic to PDF and SVG
 mkdir -p "${OUTPUT_DIR}"
 kicad-cli sch export pdf -o "${OUTPUT_DIR}/schematic.pdf" "${SCHEMATIC_FILE}"
-kicad-cli sch export svg -o "${OUTPUT_DIR}/schematic.svg" "${SCHEMATIC_FILE}"
+#kicad-cli sch export svg -o "${OUTPUT_DIR}/schematic.svg" "${SCHEMATIC_FILE}"
 
